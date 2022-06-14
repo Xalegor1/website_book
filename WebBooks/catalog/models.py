8ed17c6ec3f1482a3e66e2b1cf806a0e1e1cef9b
Xalegor <Argus1672@gmail.com>
2022-06-14 23:05:14 +0500

    lol

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -5,3 +5,23 @@ class Genre(models.Model):
     name = models.CharField(max_length=200, help_text='Введите жанр книги', verbose_name='Жанр книги')
 
 
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=20, help_text='Введите язык книги', verbose_name='Язык книги')   
+
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, help_text='Введите имя автора', verbose_name='Имя автора')
+    last_name = models.CharField(max_length=100, help_text='Введите фамилию автора', verbose_name='Фамилия автора')
+    date_of_birth = models.DateField(help_text='Введите дату рождения', verbose_name='Дата рождения', null=True, blank=True)
+    date_of_death = models.DateField(help_text='Введите дату смерти', verbose_name='Дата смерти', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.last_name
