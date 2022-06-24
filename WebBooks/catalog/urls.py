750e8080e45165def3196d2474453da907053a26
Xalegor <Argus1672@gmail.com>
2022-06-25 00:13:43 +0500

    Authenticate_work

--- WebBooks/catalog/urls.py
+++ WebBooks/catalog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from catalog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors')
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -19,6 +19,8 @@ class AuthorListView(generic.ListView):
     paginate_by = 3 
 
 def index(request):
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status=2).count()
@@ -27,5 +29,6 @@ def index(request):
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_visits': num_visits
     })
